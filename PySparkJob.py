4fd3f6619823637afdab5e6be3ae8c567cf49b39
Alex Shelpov <shelpov-a@mail.ru>
2020-11-20 01:15:41 +0300

    Seems have done all

--- PySparkJob.py
+++ PySparkJob.py
@@ -1,14 +1,35 @@
 import io
 import sys
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, datediff, when
 from pyspark.sql import functions as F
+from pyspark.sql.types import IntegerType
 
 
 def process(spark, input_file, target_path):
     df = spark.read.options(header="true", inferschema="true").parquet(input_file)  # read file
     df = rm_bugs(df, spark)  # delete advertisements with zero views and more than 0 clicks
     output_df = prepare_initial_df(df, spark)
+    output_df = count_ctr(df, output_df, spark)
+    train_df, test_df, validate_df = output_df.randomSplit([0.5, 0.25, 0.25], 42)
+
+    target_path = os.path.join(target_path, "result")
+    os.mkdir(target_path)
+
+    path_train = os.path.join(target_path, "train")
+    os.mkdir(path_train)
+    train_df.repartition(1).write.mode('overwrite').parquet(path_train)
+
+    path_test = os.path.join(target_path, "test")
+    os.mkdir(path_test)
+    test_df.repartition(1).write.mode('overwrite').parquet(path_test)
+
+    path_validate = os.path.join(target_path, "validate")
+    os.mkdir(path_validate)
+    validate_df.repartition(1).write.mode('overwrite').parquet(path_validate)
+
+    print("Have finished jobs")
 
 
 def rm_bugs(data_frame, spark):
@@ -58,11 +79,35 @@ def prepare_initial_df(data_frame, spark):
 
     #  aggregate DataFrame to find day_count feature and join with output_df
     uniq_date = data_frame.groupBy("ad_id").agg(F.countDistinct("date").alias("day_count"))
+    uniq_date = uniq_date.withColumn("day_count", f.col("day_count").cast(IntegerType()))
     output_df = output_df.join(uniq_date, on="ad_id", how="left")
 
     return output_df
 
 
+def count_ctr(data_frame, initial_df, spark):
+    """
+    count CTR for ads
+    """
+    data_frame.createOrReplaceTempView("df_new")
+    ctr_df = spark.sql("""
+                       SELECT tv.ad_id, tv.total_views, tc.total_clicks
+                       FROM
+                         (SELECT ad_id, count(ad_id) as total_views
+                         FROM df_new
+                         WHERE event = 'view'
+                         GROUP BY ad_id) as tv
+                       LEFT JOIN
+                         (SELECT ad_id, count(ad_id) as total_clicks
+                         FROM df_new
+                         WHERE event = 'click'
+                         GROUP BY ad_id) as tc
+                       ON tv.ad_id = tc.ad_id
+                       """)
+    ctr_df = ctr_df.withColumn("CTR", when(col("total_clicks").isNull(), 0).
+                               otherwise(col("total_clicks") / col("total_views")))
+    output_df = initial_df.alias("df1").join(ctr_df.alias("df2"), on="ad_id", how="left").select("df1.*", "df2.CTR")
+    return output_df
 
 
 def main(argv):
